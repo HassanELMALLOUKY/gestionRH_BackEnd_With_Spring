fa956c4f219b26b7e4b048b9b37c7d58bb27caba
Ibtissam <ibtissamchouklati2001@gmail.com>
2022-08-08 18:56:01 +0000

    traitement paie

--- src/main/java/com/YadouSoft/gestionRH/GestionRhApplication.java
+++ src/main/java/com/YadouSoft/gestionRH/GestionRhApplication.java
@@ -1,12 +1,7 @@
 package com.YadouSoft.gestionRH;
 
-import com.YadouSoft.gestionRH.models.Attendance;
-import com.YadouSoft.gestionRH.models.abscent;
-import com.YadouSoft.gestionRH.services.AttendanceService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GestionRhApplication {
@@ -15,23 +10,4 @@ public class GestionRhApplication {
         SpringApplication.run(GestionRhApplication.class, args);
     }
 
-
-    @Bean
-    CommandLineRunner start(AttendanceService attendanceService) {
-        return args -> {
-         //   attendanceService.addAbscent(new abscent(null, "8:00", "1:08", "19:19", "present", "justifier"));
-         //   attendanceService.addAbscent(new abscent(null, "8:00", "1:08", "19:19", "present", "justifier"));
-
-//      accountServices.addNewRole(new userRole(null,"admin"));
-//      accountServices.addNewRole(new userRole(null,"user"));
-//      accountServices.addNewRole(new userRole(null,"manager"));
-//      accountServices.addRoleToUser("rachidrachid","admin");
-//      accountServices.addRoleToUser("hassanhassan","user");
-//      accountServices.addRoleToUser("brahimbrahim","manager");
-
-
-        };
-
-
-    }
 }
